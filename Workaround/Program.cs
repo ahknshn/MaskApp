be8f085c81302102b6800a7224b4023a11189846
ahknshn <silenthakan@hotmail.com>
2024-02-12 01:25:33 +0300

    Proje dosyası ekle.

---
+++ Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.FirstName = "AHMET";
+        person1.LastName = "HAKAN";
+        person1.DateOfBirthYear = 2024;
+        person1.NationalIdentity = 111;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+
+        Console.ReadLine();
+    }
+
+}
